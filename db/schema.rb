4ad9d2ab1c706315b3e7c360c3779c85496cbaac
matsumoto978 <myoshi878@gmail.com>
2021-10-20 17:02:47 +0900

    commentモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_075758) do
+ActiveRecord::Schema.define(version: 2021_10_20_075945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "gym_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gym_id"], name: "index_comments_on_gym_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "gyms", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -58,6 +68,8 @@ ActiveRecord::Schema.define(version: 2021_10_20_075758) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "gyms"
+  add_foreign_key "comments", "users"
   add_foreign_key "gyms", "users"
   add_foreign_key "keeps", "gyms"
   add_foreign_key "keeps", "users"
