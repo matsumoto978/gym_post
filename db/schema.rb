53d171afcc67df232757b83580561c03f86f31fa
matsumoto978 <myoshi878@gmail.com>
2021-10-20 16:56:48 +0900

    likeモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_074915) do
+ActiveRecord::Schema.define(version: 2021_10_20_075314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2021_10_20_074915) do
     t.index ["user_id"], name: "index_gyms_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "gym_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gym_id"], name: "index_likes_on_gym_id"
+    t.index ["user_id", "gym_id"], name: "index_likes_on_user_id_and_gym_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +49,6 @@ ActiveRecord::Schema.define(version: 2021_10_20_074915) do
   end
 
   add_foreign_key "gyms", "users"
+  add_foreign_key "likes", "gyms"
+  add_foreign_key "likes", "users"
 end
