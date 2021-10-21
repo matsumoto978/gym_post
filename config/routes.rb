265ac6b233d5f5dc6a0297c47e7ce6bd5e0468cf
matsumoto978 <myoshi878@gmail.com>
2021-10-22 00:28:08 +0900

    ゲストログイン処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root "gyms#index"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
